5c61bf765edb3b50b859537c9df318a1b0032bb0
Isuru Fernando <isuruf@gmail.com>
2024-04-23 17:01:46 -0500

    try win

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,9 @@
 set -euo pipefail
 
 mkdir -p $PREFIX/lib
-cp $BUILD_PREFIX/share/gnuconfig/config.* build-aux/
+if [[ "$target_platform" != win-* ]]; then
+  cp $BUILD_PREFIX/share/gnuconfig/config.* build-aux/
+fi
 
 if [[ "$target_platform" == osx-* ]]; then
   # This breaks the tests when the build is run as root (as on CI)
