11dd9e2a8768808ff1273eaa2d720fa665cd3eb2
Isuru Fernando <isuruf@gmail.com>
2024-04-23 22:43:46 -0500

    use m2w64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,11 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
-if %ERRORLEVEL% neq 0 exit 1
+copy "%RECIPE_DIR%\build.sh" .
+set PREFIX=%LIBRARY_PREFIX:\=/%
+set SRC_DIR=%SRC_DIR:\=/%
+set MSYSTEM=MINGW%ARCH%
+set MSYS2_PATH_TYPE=inherit
+set CHERE_INVOKING=1
+
+bash -lc "./build.sh"
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+exit /b 0
